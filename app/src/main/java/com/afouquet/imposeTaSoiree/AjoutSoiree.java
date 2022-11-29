71fceeb5424fa75c4d7733633e0e9a489ace824e
afouquet <afouquet67@gmail.com>
2022-11-29 16:56:07 +0100

    création activity ajout soirée

---
+++ app/src/main/java/com/afouquet/imposeTaSoiree/AjoutSoiree.java
@@ -0,0 +1,14 @@
+package com.afouquet.imposeTaSoiree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjoutSoiree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_soiree);
+    }
+}
